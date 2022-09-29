da460e3baebd2934ac52340a3184ad881d3f2ab8
Neil Pie <neil@pie.co.de>
2022-09-29 17:14:02 +0100

    first commit

---
+++ plugin.php
@@ -0,0 +1,8 @@
+<?php 
+/*
+Plugin Name: Pie Plugin Template
+Description: This plugin doesn't do anything, but gives you a very minimal starting template.
+Version: 0.0.1
+Author: The team at PIE
+Author URI: https://pie.co.de
+*/
